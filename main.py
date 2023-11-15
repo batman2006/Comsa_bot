a3a448cf11c0a96df65811387e7f41ca5fc870ac
batman2006 <nazarworker17@gmail.com>
2023-11-15 19:41:58 +0200

    currency select update

--- main.py
+++ main.py
@@ -18,14 +18,14 @@ def handle_callback(call):
 
     if call.data == 'back':
         if form_status == 'quantity':
-            bot.send_message(chat_id, "Обери криптовалюту , яку хочеш купити або можеш перевірити свій баланс🤝", reply_markup=start_markup)
+            bot.send_message(chat_id, "Обери криптовалюту, яку хочеш купити або можеш перевірити свій баланс🤝", reply_markup=start_markup)
             form_status = None
         elif form_status == 'address':
             chosen_coin = user_data[chat_id]['coin']
             bot.send_message(chat_id, f"Обрано {chosen_coin}. Введіть потрібну кількість 🔢", reply_markup=quantity_markup)
             form_status = 'quantity'
         elif form_status == 'profile':
-            bot.send_message(chat_id, "Обери криптовалюту , яку хочеш купити або можеш перевірити свій баланс🤝", reply_markup=start_markup)
+            bot.send_message(chat_id, "Обери криптовалюту, яку хочеш купити або можеш перевірити свій баланс🤝", reply_markup=start_markup)
             form_status = None
     elif call.data in ['BNB', 'TRX', 'MATIC', 'SOL', 'ETH', 'SUI', 'APTOS']:
         bot.send_message(chat_id, f"Обрано {call.data}. Введіть потрібну кількість 🔢", reply_markup=quantity_markup)
@@ -34,11 +34,15 @@ def handle_callback(call):
     elif call.data == 'profile':
         bot.send_message(chat_id, "Тут ви можете переглядати інформацію про себе 📱", reply_markup=profile_markup)
         form_status = 'profile'
-    elif call.data in ['grn','usd']:
-        chosen_coin = call.data
-        bot.send_message(chat_id, f"Обрано {chosen_coin}. Введіть вартість потрібної вам кількості в {chosen_coin}", reply_markup=quantity_markup)
+    elif call.data in ['grn', 'usd']:
+        chosen_coin = user_data[chat_id]['coin']  # Retrieve the chosen coin from user_data
+        if call.data == 'grn':
+            bot.send_message(chat_id, f"Обрано гривні. Введіть вартість потрібної вам кількості в гривнях", reply_markup=quantity_markup)
+        elif call.data == 'usd':
+            bot.send_message(chat_id, f"Обрано долари. Введіть вартість потрібної вам кількості в доларах", reply_markup=quantity_markup)
         user_data[chat_id] = {'coin': chosen_coin, 'status': 'filling'}
         form_status = "quantity"
+
 
 @bot.message_handler(func=lambda message: message.chat.id in user_data and form_status == "quantity")
 def handle_quantity(message):
